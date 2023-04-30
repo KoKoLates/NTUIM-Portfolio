7db09df69b7c8d502ad030324cc53625658cae01
KoKoLates <the21515@gmail.com>
2023-04-30 18:40:55 +0800

    update image card

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,11 @@
 
 import './footer.css';
+import { useRef } from 'react';
 import image from '../images/footer.jpg';
 
 function Footer() {
+    const link = 'https://github.com/KoKoLates';
+
     return (
         <footer className='footer-container'>
             <div className='footer-image-container'>
@@ -15,13 +18,22 @@ function Footer() {
                         here to contact
                     </span>
                 </div>
-                <a className='footer-interact-button' href="#cover">
-                    <div className='footer-button'>
-                        <span>CONTACT</span>
-                    </div>
-                </a>
+                <div className='test'>
+                    <ContactButton reff={link} name='CONTACT' />
+                    {/* <ContactButton /> */}
+                </div>
             </div>
         </footer>
+    )
+}
+
+const ContactButton = (props) => {
+    const {reff, name} = props;
+    return (
+        <a className='footer-interact-button button' href={reff} target='_blank'>
+            <div className='footer-button'></div>
+            <span>{name}</span>
+        </a>
     )
 }
 
--- src/pages/home/library.jsx
+++ src/pages/home/library.jsx
@@ -3,22 +3,27 @@ import './library.css';
 import images from "../../images/image";
 
 function Library() {
+    const i = images[0];
+    const m = images[1]
     return (
         <div className="library-container">
-            {
-                images.map((image, idx) => {
-                    return ImageCardContainer(image, idx);
-                })
-            }
+            <ImageContainer image={i} year='2020' text='test images' />
+            <ImageContainer image={m} year='2020' text='test images' />
         </div>
     )
 }
 
-function ImageCardContainer(image, idx) {
+const ImageContainer = (props) => {
+    const {image, year, text} = props;
 
     return (
-        <div key={idx}>
-            <img src={image} alt="" />
+        <div className='image-container'>
+            <img src={image} alt="" className='is-inview' />
+            <div className='image-info-container'>
+                <p>{year}</p>
+                <p>{text}</p>
+                <p className='image-info-title'>Testing</p>
+            </div>
         </div>
     )
 }
